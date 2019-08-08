26d698bff078b1aa82928cee3e15bfd67952b405
OlaStalberg <olastaalberg@gmail.com>
2019-08-08 12:58:17 +0200

    made buttons work

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Link from "@material-ui/core/Link";
+import Button from '@material-ui/core/Button';
 
 function NavigationApp() {
     const [url, setURL] = useState('https://play-with-fint.felleskomponent.no/utdanning/elev/person/fodselsnummer/14029923273');
@@ -73,7 +74,79 @@ function Header() {
     );
 }
 
-ReactDOM.render(<NavigationApp/>, document.getElementById('root'));
+function NavigationApp2() {
+    const [url, setURL] = useState('https://play-with-fint.felleskomponent.no/utdanning/elev/person/fodselsnummer/14029923273');
+    const [json, setJson] = useState('');
+
+    useEffect(() => {
+        console.log("useEffect Triggered");
+        console.log(url);
+        fetch(url)
+            .then(res => res.json())
+            .then((result) => {
+                    setJson(result);
+                }
+            )
+    }, [url]);
+
+    function Data() {
+        let collection = [];
+
+        function getData(json) {
+            for (const key in json) {
+                if (key != "_links") {
+                    if (Array.isArray(json[key])) {
+                        console.log(key + json[key]);
+                        collection.push(key);
+                        for (const entry in json[key]) {
+                            collection.push(<div>{getData(json[key])}</div>)
+                        }
+                    } else if (typeof json[key] == "object") {
+                        collection.push(<div>{key}</div>);
+                        collection.push(<div>{getData(json[key])}</div>);
+                    } else {
+                        (!Array.isArray(json)) ?
+                            collection.push(<div>{key}: {json[key]}</div>) : collection.push(<div>{json[key]}</div>);
+                    }
+                }
+            }
+        }
+
+        getData(json);
+        return collection;
+    }
+
+
+    function Links() {
+        let linksCollection = [];
+        if (json.hasOwnProperty("_links")) {
+            const links = json._links;
+            for (let key in links) {
+                for (let i = 0; i < links[key].length; i++) {
+                    if (links[key][i].hasOwnProperty("href")) {
+                        linksCollection.push(<div>
+                            <p><Button color={"secondary"}
+                                       onClick={() => setURL(links[key][i].href)}>{key}: {links[key][i].href}</Button>
+
+                            </p>
+                        </div>);
+                    }
+                }
+
+            }
+        }
+        return <div>{linksCollection}</div>;
+
+    }
+
+
+    return (<div>
+        <Data></Data>
+        <Links></Links>
+    </div>);
+}
+
+ReactDOM.render(<NavigationApp2/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
